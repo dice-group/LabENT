202d5b09e3cf00d44a276c1b565f1f0ca7afe855
Hamada Zahera <85758022+hzahera@users.noreply.github.com>
2022-02-28 23:36:34 +0100

    Update datadump.sql

--- LabENT-docker/mysql-dump/datadump.sql
+++ LabENT-docker/mysql-dump/datadump.sql
@@ -1,3 +1,8 @@
+CREATE TABLE IF NOT EXISTS wordpress.annotationData (
+`ID` VARCHAR(11) NOT NULL,
+`Triples` TEXT NOT NULL, 
+`Types` VARCHAR(50));
+
 DELETE FROM wordpress.annotationData;
 LOAD DATA LOCAL INFILE '/docker-entrypoint-initdb.d/annotationData.csv'
 INTO TABLE wordpress.annotationData 
