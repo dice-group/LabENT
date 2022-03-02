b961b380664d602bb7c7c7b2d9c13c8caf33737e
Hamada Zahera <85758022+hzahera@users.noreply.github.com>
2022-03-02 19:52:46 +0100

    chown for wordpress to resolve linux persmission

--- script-lymphograph.sh
+++ script-lymphograph.sh
@@ -5,3 +5,6 @@ mv Lymphography/preprocessing/annotationData.csv LabENT-docker/mysql-dump/annota
 cd LabENT-docker/mysql-dump
 cat datadump.sql | docker exec -i labent-docker_db_1 mysql --host 0.0.0.0 --port 3306 -uroot -pwordpress wordpress
 
+chown www-data:www-data -R LabENT/LabENT-docker/wp-content
+
+
