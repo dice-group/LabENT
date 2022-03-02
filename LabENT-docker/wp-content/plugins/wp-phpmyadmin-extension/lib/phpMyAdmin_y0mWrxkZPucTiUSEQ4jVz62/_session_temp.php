d3a610fb0ac9e4c1111fed49b8a7cb770bcba428
hzahera <hamada.zahera@uni-paderborn.de>
2022-03-02 10:49:20 +0100

    update LabENT from Linux machine

--- LabENT-docker/wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_y0mWrxkZPucTiUSEQ4jVz62/_session_temp.php
+++ LabENT-docker/wp-content/plugins/wp-phpmyadmin-extension/lib/phpMyAdmin_y0mWrxkZPucTiUSEQ4jVz62/_session_temp.php
@@ -1,1 +1,1 @@
-<?php $sess_vars = ["time"=>1646156214, "name"=>"wp_pma_4wnCAYe7rugHXy",  "value"=>"wp_pma_I4o5aXFeVdPOrCtwy0nbYGx",  "require_ip"=>true, "ip"=>"172.18.0.1", "strip_slashes"=>true];
+<?php $sess_vars = ["time"=>1646213828, "name"=>"wp_pma_w9icp8y2Te7uQf",  "value"=>"wp_pma_307ACeiL5ZdaqyBS1PJrjIU",  "require_ip"=>true, "ip"=>"172.18.0.1", "strip_slashes"=>true];
